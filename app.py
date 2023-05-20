a51d6f5e6b213dafedec349f20de161b6423bab4
Jerico Delos Reyes <jericodlsrys@gmail.com>
2023-05-20 14:41:53 +0800

    made colored

--- app.py
+++ app.py
@@ -39,6 +39,23 @@ from PIL import Image, ImageOps
 import numpy as np
 import tensorflow as tf
 
+
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+
+# def import_and_predict(image_data,model):
+#     size=(128,128)
+#     image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+#     img=np.asarray(image)
+#     img_reshape=img[np.newaxis,...]
+#     prediction=model.predict(img_reshape)
+#     return prediction
+
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+
 def import_and_predict(image_data, model):
     size = (128, 128)
     
@@ -50,29 +67,13 @@ def import_and_predict(image_data, model):
     # Convert the image to grayscale if necessary
     if img.ndim == 3 and img.shape[2] == 3:
         img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    
-    # Ensure the image has 3 channels
-    img_reshape = np.repeat(img[:, :, np.newaxis], 3, axis=2)
-    
-    # Reshape the image to match the input shape of the model
-    img_reshape = img_reshape.reshape((1,) + img_reshape.shape)
-    
-    # Clear the previous TensorFlow graph
-    tf.keras.backend.clear_session()
-    
-    # Create a new TensorFlow graph
-    graph = tf.Graph()
-    
-    with graph.as_default():
-        # Load the model within the new graph
-        with tf.Session(graph=graph):
-            # Make predictions using the Keras model
-            prediction = model.predict(img_reshape)
-    
+
+    # Reshape the image to add a channel dimension
+    img_reshape = img.reshape((1,) + img.shape + (1,))
+
+    # Make predictions using the Keras model
+    prediction = model.predict(img_reshape)
     return prediction
-
-
-
 
 if file is None:
     st.text("Please upload an image file")
