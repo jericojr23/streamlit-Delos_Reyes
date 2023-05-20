9c979a60786c1814c19be226478fe99052101d72
Jerico Delos Reyes <jericodlsrys@gmail.com>
2023-05-20 14:25:32 +0800

    made colored

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 @st.cache_resource
 def load_model():
     tf.keras.backend.clear_session()  # Clear the TensorFlow graph
-    model = tf.keras.models.load_model('newer_newer_model_final.h5')
+    model = tf.keras.models.load_model('newer_newer_model.h5')
     return model
 
 model = load_model()
@@ -17,13 +17,42 @@ file=st.file_uploader("Choose plant photo from computer",type=["jpg","png"])
 import cv2
 from PIL import Image,ImageOps
 import numpy as np
-def import_and_predict(image_data,model):
-    size=(128,128)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+# def import_and_predict(image_data,model):
+#     size=(128,128)
+#     image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+#     img=np.asarray(image)
+#     img_reshape=img[np.newaxis,...]
+#     prediction=model.predict(img_reshape)
+#     return prediction
+
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+
+def import_and_predict(image_data, model):
+    size = (128, 128)
+    
+    # Resize the image to the expected input shape of the model
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    img = np.asarray(image)
+    img = cv2.resize(img, (128, 128), interpolation=cv2.INTER_NEAREST)
+    
+    # Convert the image to grayscale if necessary
+    if img.ndim == 3 and img.shape[2] == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    
+    # Ensure the image has 3 channels
+    img_reshape = np.repeat(img[:, :, np.newaxis], 3, axis=2)
+    
+    # Reshape the image to match the input shape of the model
+    img_reshape = img_reshape.reshape((1,) + img_reshape.shape)
+
+    # Make predictions using the Keras model
+    prediction = model.predict(img_reshape)
     return prediction
+
+
+
 if file is None:
     st.text("Please upload an image file")
 else:
