3d2fc1a5c7e0466dcdc5c4234f81e8786d532f8b
jericojr23 <jericodlsrys@gmail.com>
2023-10-11 23:39:06 +0700

    changed image variable

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def import_and_predict(image_data, model):
     size = (128, 128)
 
     # Resize the image to the expected input shape of the model
-    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    image = ImageOps.fit(image_data, size, Image.LANCZOS)
     img = np.asarray(image)
     img = cv2.resize(img, (128, 128), interpolation=cv2.INTER_NEAREST)
 
